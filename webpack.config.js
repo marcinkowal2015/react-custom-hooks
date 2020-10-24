224e35b58bf6ec2ef197a96ea915a6f6c50b0366
Marcin Kowal <marcin.kowal@medius.com>
2020-10-24 22:53:55 +0200

    Initial commit for react custom hooks repo

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'production',
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+        libraryTarget: 'commonjs2'
+    },
+    externals: {
+        // Don't bundle react or react-dom      
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "ReactDOM",
+            root: "ReactDOM"
+        }
+    }
+}
